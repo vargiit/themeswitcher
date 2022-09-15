e687075feaf3b7ecd0b545913cc45245e8b774b9
vargiit <lakshmijayram4@gmail.com>
2022-09-15 19:40:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactSwitch from "react-switch";
+import "./App.css";
+import Form from "./components/Form";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
 
 function App() {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id={theme}>
+      <Form />
+      <div className="switch">
+        <ReactSwitch onChange={toggleTheme} checked={theme === "light"} />
+      </div>
     </div>
   );
 }
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+const useTheme = () => useContext(ThemeContext);
+console.log(useTheme);
+export { useTheme, ThemeProvider };
